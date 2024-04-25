93ba844c547e23bac942ef971ef331738c81f4a9
Chris Uzoh <genbliz@gmail.com>
2024-04-26 00:13:15 +0100

    updated vals

--- matching-pairs/index.ts
+++ matching-pairs/index.ts
@@ -44,8 +44,6 @@ function sockMerchant(n: number | string, arr: string | number[]) {
   }
 
   return pairsCount;
-
-  // 9, [10, 20, 20, 10, 10, 30, 50, 10, 20]
 }
 
-console.log(sockMerchant(9, "10 20 20 10 10 30 50 10 20"));
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
